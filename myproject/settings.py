6c15245c2319c530569d5b16916e5d0752ec0f96
vadimaland <vadim.aland@me.com>
2025-03-13 12:30:55 +0200

    update 13.03.2025 12:30

--- myproject/settings.py
+++ myproject/settings.py
@@ -119,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-US'
+LANGUAGE_CODE = 'et-ee'
 
 TIME_ZONE = 'Europe/Tallinn'
 
