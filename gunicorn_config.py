991ecd4a78d3b068a23df29e821ba3dcc0ca708e
vadimaland <vadim.aland@me.com>
2025-03-27 16:49:11 +0200

    update 27.03.2025 16:00

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,6 @@
 bind = "10.233.1.13:8000"
-workers = 4
-threads = 2
+workers = 8
+threads = 4
 accesslog = "/home/f/log/gunicorn/access.log"
 errorlog = "/home/f/log/gunicorn/error.log"
 loglevel = "info"
