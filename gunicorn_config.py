cd70473cc8c3906ad08edaf17de1f9a386c877ed
vadimaland <vadim.aland@me.com>
2025-05-07 16:14:38 +0300

    update 07.05.2025 16:00

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 bind = "10.233.1.13:8000"
-workers = 4
-threads = 2
-worker_class = "gthread"
+workers = 8
+threads = 1
+worker_class = "sync"
 accesslog = "/home/f/log/gunicorn/access.log"
 errorlog = "/home/f/log/gunicorn/error.log"
 loglevel = "info"
